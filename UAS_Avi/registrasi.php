711260becc43ba5b7bb33873f1c6d77e5e974422
aviliairs <135019967+aviliairs@users.noreply.github.com>
2023-05-31 09:27:58 +0700

    Add files via upload

---
+++ UAS_Avi/registrasi.php
@@ -0,0 +1,65 @@
+<?php
+
+include 'koneksi.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $username = mysqli_real_escape_string($koneksi, $_POST['username']);
+   $email = ($_POST['usermail']);
+   $pass = ($_POST['password']);
+   $cpass = ($_POST['cpassword']);
+
+   $select = " SELECT * FROM tbl_login WHERE username = '$username' && email = '$email' && password = '$pass'";
+
+   $result = mysqli_query($koneksi, $select);
+
+   if(mysqli_num_rows($result) > 0){
+      $error[] = 'user already exist';
+   }else{
+      if($pass != $cpass){
+         $error[] = 'password not mathched!';
+      }else{
+         $insert = "INSERT INTO tbl_login(username, email, password) VALUES('$username','$email','$pass')";
+         mysqli_query($koneksi, $insert);
+         header('location:index.php');
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3 class="title">register now</h3>
+      <?php
+         if(isset($error)){
+            foreach($error as $error){
+               echo '<span class="error-msg">'.$error.'</span>';
+            }
+         }
+      ?>
+      <input type="text" name="username" placeholder="enter your username" class="box" required>
+      <input type="email" name="usermail" placeholder="enter your email" class="box" required>
+      <input type="password" name="password" placeholder="enter your password" class="box" required>
+      <input type="password" name="cpassword" placeholder="confirm your password" class="box" required>
+      <input type="submit" value="register now" class="form-btn" name="submit">
+      <p>already have an account? <a href="index.php">login now!</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
